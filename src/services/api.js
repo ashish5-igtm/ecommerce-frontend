365377f27631fc22c338dfe81725b42fcae5aa8d
ashish5-igtm <ashishpala456@gmail.com>
2026-03-30 15:34:52 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ecommerce-backend-production-3330.up.railway.app"
+  baseURL: "https://ecommerce-backend-production-0395.up.railway.app"
 });
 
 export default API;
